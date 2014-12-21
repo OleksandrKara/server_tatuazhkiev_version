1250076b663e41da9149783e3cb85c4a20583833
Oleksandr Kara <Oleksandr_Kara@epam.com>
2014-12-21 22:16:17 +0200

    New images added and editor added

--- tatuazhkiev/articles/models.py
+++ tatuazhkiev/articles/models.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from tinymce.models import HTMLField
 
 class Post(models.Model):
     title = models.CharField(max_length=255) # заголовок поста
     datetime = models.DateTimeField(u'Дата публикации') # дата публикации
     short_description = content = models.TextField(max_length=10000) # малый текст поста
-    content = models.TextField(max_length=10000) # текст поста
+    content = HTMLField() # текст поста
 
     def __unicode__(self):
         return self.title
