1157af99ae3d38a94ba4db55a95cd180b7080943
Oleksandr Kara <Oleksandr_Kara@epam.com>
2015-01-18 18:12:31 +0200

    Meta tags are added as a flatpage field. Flatpage module was extended

--- tatuazhkiev/settings.py
+++ tatuazhkiev/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'tatuazhkiev.fotos',
+	'tatuazhkiev.flatpages',
 	'sorl.thumbnail',
 	'django.contrib.sitemaps',
 	'django.contrib.flatpages',
