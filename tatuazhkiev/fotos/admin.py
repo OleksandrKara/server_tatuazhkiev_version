cfff98614c4eaa7ec1850c3594ccb8c33fe352a3
Oleksandr Kara <Oleksandr_Kara@epam.com>
2015-03-09 23:49:47 +0200

    Images added to admin

--- tatuazhkiev/fotos/admin.py
+++ tatuazhkiev/fotos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from tatuazhkiev.fotos.models import Foto
 
 class FotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'date')
-    search_fields = ('title', 'date')
+    list_display = ('title', 'type', 'date', 'image_tag')
+    search_fields = ('title', 'date','image_tag')
 
 admin.site.register(Foto, FotoAdmin)
--- tatuazhkiev/fotos/models.py
+++ tatuazhkiev/fotos/models.py
@@ -12,4 +12,12 @@ class Foto(models.Model):
 	comment_second = models.CharField(max_length=300)
 	type = models.CharField(max_length=2, default='Br', choices=TATUAZH_TYPE)
 	image_location = models.ImageField(upload_to="images/", help_text='Please upload your photos here')
-	date = models.DateTimeField(auto_now_add=True)
+	date = models.DateTimeField(auto_now_add=True, editable=True)
+	
+	def image_tag(self):
+		if self.image_location:
+			return u'<img src="%s" width="100"/>' % self.image_location.url
+		else:
+			return '(none)'
+	image_tag.short_description = 'Image'
+	image_tag.allow_tags = True
