66a53dc58705d6b8debee6bceb7e93543aa54949
Oleksandr Kara <Oleksandr_Kara@epam.com>
2015-03-11 11:28:53 +0200

    admin part was updated

--- tatuazhkiev/fotos/admin.py
+++ tatuazhkiev/fotos/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from tatuazhkiev.fotos.models import Foto
 
 class FotoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'type', 'date', 'image_tag')
+    list_display = ('title', 'type', 'image_tag', 'date')
     search_fields = ('title', 'date')
 
 admin.site.register(Foto, FotoAdmin)
