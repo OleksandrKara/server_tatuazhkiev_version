2b30a3622d0436de2c22037a1a853ff7eeb2332c
Oleksandr Kara <Oleksandr_Kara@epam.com>
2015-04-19 17:02:19 +0300

    settings are changed

--- tatuazhkiev/settings.py
+++ tatuazhkiev/settings.py
@@ -21,13 +21,13 @@ SECRET_KEY = '06&(uw0lsobw9(3i@ajnfuf_lm#wbj+v6ph+)!g@7s(+tm84*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-#DEBUG = False
-#TEMPLATE_DEBUG = False
-#THUMBNAIL_DEBUG = False
+DEBUG = False
+TEMPLATE_DEBUG = False
+THUMBNAIL_DEBUG = False
 
-DEBUG = True
-TEMPLATE_DEBUG = True
-THUMBNAIL_DEBUG = True
+#DEBUG = True
+#TEMPLATE_DEBUG = True
+#THUMBNAIL_DEBUG = True
 
 ALLOWED_HOSTS = ['tatuazhkiev.com.ua','127.0.0.1:8000','localhost:8000']
 
